d186030a28be5f944cb09fec50e8d4efd7750508
Vojtech Cahlik <mail@cahlikvojtech.com>
2019-12-20 11:06:35 +0100

    Perform ANN heuristic query distribution analysis, plot more figures

--- prototype/prototype/experiments/misc/ann_heuristic_query_distribution.py
+++ prototype/prototype/experiments/misc/ann_heuristic_query_distribution.py
@@ -17,7 +17,7 @@ ida_star_global.heuristic = pdb_global
 
 
 def callback(board):
-    path = constants.PROJECT_ROOT + "/data/experiments/ann-heuristic-query-distribution.csv"
+    path = constants.PROJECT_ROOT + "/data/experiments/ann-heuristic-keras-1024-1024-512-128-64-v3-query-distribution.csv"
     with open(path, 'a') as f:
         if random.random() < 0.001:
             ida_star_global.run(board)
@@ -27,18 +27,15 @@ def callback(board):
 
 
 if __name__ == "__main__":
-    path = constants.PROJECT_ROOT + "/data/experiments/ann-heuristic-query-distribution.csv"
+    path = constants.PROJECT_ROOT + "/data/experiments/ann-heuristic-keras-1024-1024-512-128-64-v3-query-distribution.csv"
     with open(path, 'w') as f:
         f.write("OPTIMAL_SOLUTION_COST\n")
 
     boards_generator = RandomBoardsGenerator(4)
     a_star = AStarSearch()
 
-    def asymmetric_mean_squared_error_02(y_true, y_pred):
-        return K.mean(K.square(y_pred - y_true) * K.square(K.sign(y_pred - y_true) + 0.2), axis=-1)
-
-    model_path = os.path.join(constants.PROJECT_ROOT, 'data/neural-networks/keras-1024-1024-512-128-64-amse02.h5')
-    annh = ANNHeuristic(model_path, "AMSE0.2", asymmetric_mean_squared_error_02, callback=callback)
+    model_path = os.path.join(constants.PROJECT_ROOT, 'data/neural-networks/v3/keras-1024-1024-512-128-64-v3.h5')
+    annh = ANNHeuristic(model_path, "MSE", callback=callback)
 
     a_star.heuristic = annh
 
